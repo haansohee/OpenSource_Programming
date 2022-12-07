c6147566ef90a7bd53c5747d85333cc90479958c
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2022-12-08 01:44:27 +0900

    피보나치, 팩토리얼, 소수여부

--- hsh_202144020.go
+++ hsh_202144020.go
@@ -11,15 +11,45 @@ import (
 
 // 팩토리얼 수로 변환하는 함수
 // 팩토리얼 -> n!
+// func Change(input int) int {
+// 	var result int = 1
+// 	var i int
+
+// 	// fmt.Println("Change 로 넘어온 수 : ", input)
+
+// 	for i = 1; i <= input; i++ { // 팩토리얼 계산
+// 		result *= i
+// 		// fmt.Println(result)
+// 	}
+
+// 	return result
+// }
+
+// 피보나치 수열로 변환 (f(n) = f(n-1) + f(n-2))
 func Change(input int) int {
-	var result int = 1
+	// 재귀호출을 이용한 피보나치 수열
+	// if input <= 2 {
+	// 	return 1
+	// } else {
+	// 	return Change(input-1) + Change(input-2)
+	// }
+
 	var i int
+	var last1 int
+	var last2 int
+	var result int
 
-	// fmt.Println("Change 로 넘어온 수 : ", input)
+	if input <= 2 {
+		return 1
+	}
 
-	for i = 1; i <= input; i++ { // 팩토리얼 계산
-		result *= i
-		// fmt.Println(result)
+	last1 = 1
+	last2 = 1
+
+	for i = 2; i < input; i++ {
+		result = last1 + last2
+		last1 = last2
+		last2 = result
 	}
 
 	return result
@@ -68,11 +98,16 @@ func main() {
 		changeNum := Change(number)
 		prime := Check(number)
 
-		if prime {
-			fmt.Printf("%d 팩토리얼 수 : %d / 소수여부 : true \n", number, changeNum)
-		} else {
-			fmt.Printf("%d 팩토리얼 수 : %d / 소수여부 : false \n", number, changeNum)
+		// if prime {
+		// 	fmt.Printf("%d 팩토리얼 수 : %d / 소수여부 : true \n", number, changeNum)
+		// } else {
+		// 	fmt.Printf("%d 팩토리얼 수 : %d / 소수여부 : false \n", number, changeNum)
+		// }
 
+		if prime {
+			fmt.Printf("%d 피보나치 수 : %d / 소수여부 : true \n", number, changeNum)
+		} else {
+			fmt.Printf("%d 피보나치 수 : %d / 소수여부 : false \n", number, changeNum)
 		}
 	}
 
